90021b6f2b26e55f906b704e12564461b9db3c75
Viktor Shershnov <viktor.shershnov@quipu.de>
2023-03-02 16:10:19 +0200

    Basics of ASP.NET MVC has been studied

---
+++ Controllers/AppController.cs
@@ -0,0 +1,53 @@
+﻿using DutchTreat.Services;
+using DutchTreat.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace DutchTreat.Controllers
+{
+    public class AppController : Controller
+    {
+        private readonly IMailService _mailService;
+
+        public AppController(IMailService mailService)
+        {
+            this._mailService = mailService;
+        }
+        public IActionResult Index()
+        {
+            //throw new InvalidProgramException("Some bad things happened!");
+            return View();
+        }
+        [HttpGet("contact")]
+        public IActionResult Contact()
+        {
+            ViewBag.Title = "Contact Us";
+           // throw new InvalidProgramException("Some bad things happened!");
+            return View();
+        }
+         
+        [HttpPost("contact")]
+        public IActionResult Contact(ContactViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                //send the email
+                _mailService.SendMessage(model.Email, model.Subject, model.Message);
+                ViewBag.UserMessage = "Mail Sent";
+                ModelState.Clear();
+            }
+            else
+            {
+                //show the errors
+            }
+
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewBag.Title = "About Us";
+            return View();
+        }
+    }
+}
